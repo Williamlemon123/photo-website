023d460dbe70be5493801b033cff31dc69b60e5e
Williamlemon123 <94806020+Williamlemon123@users.noreply.github.com>
2023-03-28 13:13:38 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import Homepage from './pages/Homepage';
+import "./styles/style.css"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
